2842752231d04cc33412a9075972524f8a06dd60
Xin He <Xin.He@ingenta.com>
2016-05-09 15:11:13 +0100

    first mvn checkin

---
+++ com.ingenta.integration-test/src/main/java/com/ingenta/com/ingenta/integration_test/App.java
@@ -0,0 +1,13 @@
+package com.ingenta.com.ingenta.integration_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.ingenta.integration-test/src/test/java/com/ingenta/com/ingenta/integration_test/MavenTests.java
@@ -0,0 +1,46 @@
+package com.ingenta.com.ingenta.integration_test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class MavenTests {
+
+	@Test
+	public void browserStack1() throws MalformedURLException {
+		String USERNAME = "xinhe1";
+		String AUTOMATE_KEY = "myHB39JW5AqqzGMhAzzT";
+		String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub.browserstack.com/wd/hub";
+//		String URL = "https://hub.browserstack.com/wd/hub";
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+//	    caps.setCapability("browser", "IE");
+//	    caps.setCapability("browser_version", "7.0");
+//	    caps.setCapability("os", "Windows");
+//	    caps.setCapability("os_version", "XP");
+	    caps.setCapability("browserstack.debug", "true");
+	    
+	    caps.setCapability("browser", "Chrome");
+	    caps.setCapability("browser_version", "50.0");
+	    caps.setCapability("os", "OS X");
+	    caps.setCapability("os_version", "Mavericks");
+	    caps.setCapability("resolution", "1920x1080");
+
+	    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+	    driver.get("http://www.google.com");
+	    WebElement element = driver.findElement(By.name("q"));
+
+	    element.sendKeys("Yang Mi");
+	    element.submit();
+
+	    System.out.println(driver.getTitle());
+	    driver.quit();
+	}
+	
+}
